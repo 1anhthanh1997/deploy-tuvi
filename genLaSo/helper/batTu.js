b125c6b747a71bf46a1695ac2d3fdb4b1d14f6cd
Nguyen Chi Thanh <3anhthanh1997@gmail.com>
2025-07-24 00:04:34 +0700

    cal score

--- genLaSo/helper/batTu.js
+++ genLaSo/helper/batTu.js
@@ -14,6 +14,7 @@ const {
   getThanSat,
   getNextDay,
   getGioSinhIndex,
+  checkNguHanhRelationshipDetailed,
 } = require("./amDuong.js");
 
 // Helper function to get index with modulo
@@ -31,6 +32,13 @@ const calculateEnergyScore = (can, score, nguHanhScore) => {
   } else {
     duong.scoreDuong += score;
   }
+  return nguHanhScore;
+};
+
+const calculateFromNguHanhId = (nguHanhId, score, nguHanhScore) => {
+  let nguHanh = nguHanhScore.find((item) => item.id === nguHanhId);
+  nguHanh.scoreAm += score / 2;
+  nguHanh.scoreDuong += score / 2;
   return nguHanhScore;
 };
 
@@ -99,13 +107,212 @@ const getCanTangScore = (diaChi, nguHanhScore) => {
   return nguHanhScore;
 };
 
+const getDacViScore = (can, chi, nguHanhScore) => {
+  let nguHanhCan = thienCan[can].nguHanh;
+  let nguHanhChi = diaChi[chi].tenHanh;
+  if (
+    nguHanhCan === nguHanhChi ||
+    checkNguHanhRelationshipDetailed(nguHanhChi, nguHanhCan) === "Sinh"
+  ) {
+    nguHanhScore = calculateEnergyScore(can, 30, nguHanhScore);
+  }
+  return nguHanhScore;
+};
+
+const isSubset = (subset, superset) => {
+  return subset.every((item) => superset.includes(item));
+};
+
+const getDacTheScore = (bazi, nguHanhScore) => {
+  let chiList = [bazi.gio.chi, bazi.ngay.chi, bazi.thang.chi, bazi.nam.chi];
+  let canList = [bazi.gio.can, bazi.ngay.can, bazi.thang.can, bazi.nam.can];
+  const dacTheChiScore = [
+    { id: 0, chiList: [1, 2], nguHanhId: 5, score: 40 },
+    { id: 1, chiList: [3, 12], nguHanhId: 2, score: 40 },
+    { id: 2, chiList: [4, 11], nguHanhId: 4, score: 40 },
+    { id: 3, chiList: [5, 10], nguHanhId: 1, score: 40 },
+    { id: 4, chiList: [6, 9], nguHanhId: 3, score: 40 },
+    { id: 5, chiList: [3, 7, 11], nguHanhId: 4, score: 60 },
+    { id: 6, chiList: [9, 1, 5], nguHanhId: 3, score: 60 },
+    { id: 7, chiList: [6, 10, 2], nguHanhId: 1, score: 60 },
+    { id: 8, chiList: [12, 4, 8], nguHanhId: 2, score: 60 },
+    { id: 9, chiList: [5, 11, 2, 8], nguHanhId: 5, score: 120 },
+    { id: 10, chiList: [3, 4, 5], nguHanhId: 2, score: 90 },
+    { id: 11, chiList: [6, 7, 8], nguHanhId: 4, score: 90 },
+    { id: 12, chiList: [9, 10, 11], nguHanhId: 1, score: 90 },
+    { id: 13, chiList: [12, 1, 2], nguHanhId: 3, score: 90 },
+  ];
+  const dacTheCanScore = [
+    { id: 0, canList: [1, 6], nguHanhId: 5, score: 20 },
+    { id: 1, canList: [2, 7], nguHanhId: 1, score: 20 },
+    { id: 2, canList: [3, 8], nguHanhId: 3, score: 20 },
+    { id: 3, canList: [4, 9], nguHanhId: 2, score: 20 },
+    { id: 4, canList: [5, 10], nguHanhId: 4, score: 20 },
+  ];
+
+  dacTheChiScore.forEach((item) => {
+    if (isSubset(item.chiList, chiList)) {
+      nguHanhScore = calculateFromNguHanhId(
+        item.nguHanhId,
+        item.score,
+        nguHanhScore
+      );
+    }
+  });
+  dacTheCanScore.forEach((item) => {
+    if (isSubset(item.canList, canList)) {
+      nguHanhScore = calculateFromNguHanhId(
+        item.nguHanhId,
+        item.score,
+        nguHanhScore
+      );
+    }
+  });
+  return nguHanhScore;
+};
+
+const calculateWithCoefficient = (chiThang, nguHanhScore) => {
+  const coefficient = [
+    {
+      id: 1, // Tháng 1 (Dần)
+      scale: [
+        { nguHanhId: 2, coefficient: 1.5 }, // Mộc
+        { nguHanhId: 5, coefficient: 0.4 }, // Thổ
+        { nguHanhId: 3, coefficient: 0.9 }, // Thủy
+        { nguHanhId: 4, coefficient: 1.3 }, // Hỏa
+        { nguHanhId: 1, coefficient: 0.7 }, // Kim
+      ],
+    },
+    {
+      id: 2, // Tháng 2 (Mão)
+      scale: [
+        { nguHanhId: 2, coefficient: 1.5 },
+        { nguHanhId: 5, coefficient: 0.4 },
+        { nguHanhId: 3, coefficient: 0.9 },
+        { nguHanhId: 4, coefficient: 1.3 },
+        { nguHanhId: 1, coefficient: 0.7 },
+      ],
+    },
+    {
+      id: 3, // Tháng 3 (Thìn)
+      scale: [
+        { nguHanhId: 2, coefficient: 1.3 },
+        { nguHanhId: 5, coefficient: 1.5 },
+        { nguHanhId: 3, coefficient: 0.4 },
+        { nguHanhId: 4, coefficient: 0.9 },
+        { nguHanhId: 1, coefficient: 0.7 },
+      ],
+    },
+    {
+      id: 4, // Tháng 4 (Tỵ)
+      scale: [
+        { nguHanhId: 2, coefficient: 0.9 },
+        { nguHanhId: 5, coefficient: 1.3 },
+        { nguHanhId: 3, coefficient: 0.7 },
+        { nguHanhId: 4, coefficient: 1.5 },
+        { nguHanhId: 1, coefficient: 0.4 },
+      ],
+    },
+    {
+      id: 5, // Tháng 5 (Ngọ)
+      scale: [
+        { nguHanhId: 2, coefficient: 0.7 },
+        { nguHanhId: 5, coefficient: 1.3 },
+        { nguHanhId: 3, coefficient: 0.7 },
+        { nguHanhId: 4, coefficient: 1.5 },
+        { nguHanhId: 1, coefficient: 0.4 },
+      ],
+    },
+    {
+      id: 6, // Tháng 6 (Mùi)
+      scale: [
+        { nguHanhId: 2, coefficient: 0.7 },
+        { nguHanhId: 5, coefficient: 1.5 },
+        { nguHanhId: 3, coefficient: 0.7 },
+        { nguHanhId: 4, coefficient: 1.3 },
+        { nguHanhId: 1, coefficient: 0.9 },
+      ],
+    },
+    {
+      id: 7, // Tháng 7 (Thân)
+      scale: [
+        { nguHanhId: 2, coefficient: 0.4 },
+        { nguHanhId: 5, coefficient: 0.9 },
+        { nguHanhId: 3, coefficient: 1.3 },
+        { nguHanhId: 4, coefficient: 0.7 },
+        { nguHanhId: 1, coefficient: 1.5 },
+      ],
+    },
+    {
+      id: 8, // Tháng 8 (Dậu)
+      scale: [
+        { nguHanhId: 2, coefficient: 0.4 },
+        { nguHanhId: 5, coefficient: 0.9 },
+        { nguHanhId: 3, coefficient: 1.3 },
+        { nguHanhId: 4, coefficient: 0.7 },
+        { nguHanhId: 1, coefficient: 1.5 },
+      ],
+    },
+    {
+      id: 9, // Tháng 9 (Tuất)
+      scale: [
+        { nguHanhId: 2, coefficient: 0.7 },
+        { nguHanhId: 5, coefficient: 1.5 },
+        { nguHanhId: 3, coefficient: 0.9 },
+        { nguHanhId: 4, coefficient: 0.4 },
+        { nguHanhId: 1, coefficient: 1.5 },
+      ],
+    },
+    {
+      id: 10, // Tháng 10 (Hợi)
+      scale: [
+        { nguHanhId: 2, coefficient: 1.3 },
+        { nguHanhId: 5, coefficient: 0.7 },
+        { nguHanhId: 3, coefficient: 1.5 },
+        { nguHanhId: 4, coefficient: 0.4 },
+        { nguHanhId: 1, coefficient: 0.9 },
+      ],
+    },
+    {
+      id: 11, // Tháng 11 (Tý)
+      scale: [
+        { nguHanhId: 2, coefficient: 1.3 },
+        { nguHanhId: 5, coefficient: 0.7 },
+        { nguHanhId: 3, coefficient: 1.5 },
+        { nguHanhId: 4, coefficient: 0.4 },
+        { nguHanhId: 1, coefficient: 0.9 },
+      ],
+    },
+    {
+      id: 12, // Tháng 12 (Sửu)
+      scale: [
+        { nguHanhId: 2, coefficient: 0.9 },
+        { nguHanhId: 5, coefficient: 1.5 },
+        { nguHanhId: 3, coefficient: 1.3 },
+        { nguHanhId: 4, coefficient: 0.7 },
+        { nguHanhId: 1, coefficient: 0.4 },
+      ],
+    },
+  ];
+
+  let scaleMonth = coefficient.find(
+    (item) => getIndex(item.id + 2) === chiThang
+  );
+  scaleMonth.scale.forEach((scale) => {
+    let nguHanh = nguHanhScore.find((item) => item.id === scale.nguHanhId);
+    nguHanh.scale = scale.coefficient;
+    nguHanh.total = (nguHanh.scoreAm + nguHanh.scoreDuong) * scale.coefficient;
+  });
+  return nguHanhScore;
+};
+
 const calcNguHanhScore = (bazi) => {
   let nguHanhScore = [
-    { id: 1, name: "Kim", scoreAm: 0, scoreDuong: 0 },
-    { id: 2, name: "Mộc", scoreAm: 0, scoreDuong: 0 },
-    { id: 3, name: "Thủy", scoreAm: 0, scoreDuong: 0 },
-    { id: 4, name: "Hỏa", scoreAm: 0, scoreDuong: 0 },
-    { id: 5, name: "Thổ", scoreAm: 0, scoreDuong: 0 },
+    { id: 1, name: "Kim", scoreAm: 0, scoreDuong: 0, total: 0, scale: 1 },
+    { id: 2, name: "Mộc", scoreAm: 0, scoreDuong: 0, total: 0, scale: 1 },
+    { id: 3, name: "Thủy", scoreAm: 0, scoreDuong: 0, total: 0, scale: 1 },
+    { id: 4, name: "Hỏa", scoreAm: 0, scoreDuong: 0, total: 0, scale: 1 },
+    { id: 5, name: "Thổ", scoreAm: 0, scoreDuong: 0, total: 0, scale: 1 },
   ];
   let { gio, ngay, thang, nam } = bazi;
   let canGio = gio.can;
@@ -116,19 +323,36 @@ const calcNguHanhScore = (bazi) => {
   let chiThang = thang.chi;
   let canNam = nam.can;
   let chiNam = nam.chi;
+  //calculate energy score
   nguHanhScore = calculateEnergyScore(canGio, 40, nguHanhScore);
   nguHanhScore = calculateEnergyScore(canNgay, 40, nguHanhScore);
   nguHanhScore = calculateEnergyScore(canThang, 40, nguHanhScore);
   nguHanhScore = calculateEnergyScore(canNam, 40, nguHanhScore);
+  //calculate can tang score
   nguHanhScore = getCanTangScore(chiGio, nguHanhScore);
-  console.log("Can tang gio:", nguHanhScore);
   nguHanhScore = getCanTangScore(chiNgay, nguHanhScore);
-  console.log("Can tang ngay:", nguHanhScore);
   nguHanhScore = getCanTangScore(chiThang, nguHanhScore);
-  console.log("Can tang thang:", nguHanhScore);
   nguHanhScore = getCanTangScore(chiNam, nguHanhScore);
-  console.log("Can tang nam:", nguHanhScore);
+  //calculate dac vi score
+  nguHanhScore = getDacViScore(canGio, chiGio, nguHanhScore);
+  nguHanhScore = getDacViScore(canNgay, chiNgay, nguHanhScore);
+  nguHanhScore = getDacViScore(canThang, chiThang, nguHanhScore);
+  nguHanhScore = getDacViScore(canNam, chiNam, nguHanhScore);
+  //calculate dac the score
+  nguHanhScore = getDacTheScore(bazi, nguHanhScore);
+  //calculate with coefficient
+  nguHanhScore = calculateWithCoefficient(chiThang, nguHanhScore);
+  //calculate by percent
+  nguHanhScore = calcNguHanhPercent(nguHanhScore);
   return nguHanhScore;
+};
+
+const calcNguHanhPercent = (nguHanhScore) => {
+  const total = nguHanhScore.reduce((sum, nh) => sum + nh.total, 0);
+  return nguHanhScore.map((nh) => ({
+    ...nh,
+    percent: total === 0 ? 0 : (nh.total / total) * 100,
+  }));
 };
 // Helper function to convert hour info
 const convertHourInfo = (baseInfo) => {
@@ -222,6 +446,7 @@ const getBaziData = (baseInfo) => {
     thang: { can: canThang, chi: getIndex(chiThang + 2) },
     nam: { can: canNam, chi: chiNam },
   });
+
   return {
     nguHanhScore,
     hour: {
